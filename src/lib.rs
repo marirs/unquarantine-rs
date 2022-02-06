1a0b33ff9886ef76f440971e6ed273bcadb1aa32
marirs <marirs@gmail.com>
2022-02-06 20:27:09 +0530

    docs update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// Inspired by:
+// - http://hexacorn.com/d/DeXRAY.pl
+// - https://github.com/brad-accuvant/cuckoo-modified/blob/master/lib/cuckoo/common/quarantine.py
 #[macro_use]
 extern crate lazy_static;
 
@@ -14,6 +17,47 @@ pub type Result<T> = std::result::Result<T, error::Error>;
 use patterns::*;
 use std::{ffi::OsStr, path::Path};
 
+/// This crate attempts to decrypt/restore/un-quarantine files from various AV / security products.
+/// The below is a comprehensive list of vendors it tries to restore quarantined files!
+/// * AhnLab (V3B)
+/// * Amiti (IFC)
+/// * ASquared (EQF)
+/// * Avast/AVG (Magic@0='-chest- ')
+/// * Avira (QUA)
+/// * Baidu (QV)
+/// * BitDefender (BDQ)
+/// * BullGuard (Q)
+/// * Cisco AMP
+/// * CMC Antivirus (CMC)
+/// * ESafe (VIR)
+/// * ESET (NQF)
+/// * F-Prot (TMP) (Magic@0='KSS')
+/// * G-Data (Q) (Magic@0=0xCAFEBABE)
+/// * K7 Antivirus (<md5>.QNT)
+/// * Kaspersky (KLQ, System Watcher's <md5>.bin)
+/// * Lavasoft AdAware (BDQ) /BitDefender files really/
+/// * Lumension LEMSS (lqf)
+/// * MalwareBytes Data files (DATA) - 2 versions
+/// * MalwareBytes Quarantine files (QUAR) - 2 versions
+/// * McAfee Quarantine files (BUP) /full support for OLE format/
+/// * Microsoft Antimalware / Microsoft Security Essentials
+/// * Microsoft Defender PC (Magic@0=0B AD|D3 45) - D3 45 C5 99 metadata + 0B AD malicious content
+/// * Microsoft Defender Mac (Magic@0=75 6E)
+/// * Panda <GUID> Zip files
+/// * Sentinel One (MAL)
+/// * Spybot - Search & Destroy 2 'recovery'
+/// * SUPERAntiSpyware (SDB)
+/// * Symantec ccSubSdk files: {GUID} files and submissions.idx
+/// * Symantec Quarantine Data files (QBD)
+/// * Symantec Quarantine files (VBN), including from SEP on Linux
+/// * Symantec Quarantine Index files (QBI)
+/// * Symantec Quarantine files on MAC (quarantine.qtn)
+/// * Total AV ({GUID}.dat) 'infected'
+/// * Total Defense (BDQ) /BitDefender files really/
+/// * TrendMicro (Magic@0=A9 AC BD A7 which is a 'VSBX' string ^ 0xFF)
+/// * QuickHeal <hash> files
+/// * Vipre (<GUID>_ENC2)
+/// * Zemana <hash> files+quarantine.db
 #[derive(Clone)]
 pub struct UnQuarantine<'a> {
     /// The detected Vendor of the quarantined file
