d5c298c6212c79b118c9e83badeae1400862a26a
marirs <marirs@gmail.com>
2022-02-06 17:58:58 +0530

    update

--- examples/main.rs
+++ examples/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{fs::OpenOptions, io::Write, process::exit};
+use std::{fs::OpenOptions, io::Write};
 use unquarantine::{error::Error, UnQuarantine};
 
 #[derive(Parser, Debug)]
